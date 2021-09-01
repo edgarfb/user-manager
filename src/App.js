89d85b1d82c0bcda4628171957d52f41e152fc7f
edgarfb <edgarfbritez@gmail.com>
2021-08-31 22:25:39 -0300

    re-build project

--- src/App.js
+++ src/App.js
@@ -3,24 +3,7 @@ import UsersAdd from "./components/UsersAdd/UsersAdd";
 import UsersList from "./components/UsersList/UsersList";
 
 function App() {
-  // Dummy data this will be replaced by the user´s inputs
-  const [users, setUsers] = useState([
-    { name: "Julieta", age: 7 },
-    { name: "Luciana", age: 34 },
-    { name: "Edgar", age: 33 },
-    { name: "Carpo", age: 56 },
-  ]);
-
-  const addNewUserHandler = () => {
-    console.log("hi");
-  };
-
-  return (
-    <div>
-      <UsersAdd />
-      <UsersList users={users} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
